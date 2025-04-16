784463dc9e72e2eae406df23cd76174acd5406de
ArfaDevelopers <aarfa.developers@gmail.com>
2025-04-17 00:46:22 +0500

    asas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  trailingSlash: true, // optional, but helps sometimes with route resolution
+  output: "standalone", // helps when deploying to custom environments like Cloudways
+  distDir: ".next", // default, but make sure .next is being built and deployed
+  // assetPrefix: '/', // Uncomment and set if your app is not in root
 };
 
 export default nextConfig;
